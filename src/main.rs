a4ce95e60085dd4c839c2f7046462fef84cf1b7c
brunozalc <brun0zalc@hotmail.com>
2025-10-14 13:23:58 -0300

    style(main): add some prints

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ fn main() {
         ("localhost".to_string(), 8081),
         ("localhost".to_string(), 8082),
     ];
-    let raft_node = RaftNode::new("A".to_string(), 8080, peers);
+    let mut raft_node = RaftNode::new("A".to_string(), 8080, peers);
 
-    println!("Raft node created with ID: {}", raft_node.id());
+    println!("Node started. ID: {}", raft_node.id());
+    println!("Run this program multiple times - state should persist!");
 }
